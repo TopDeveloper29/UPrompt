9c5525774308df8166e4ef4719bd691e5e320c2d
Hugo Beaulieu <beah@premiertech.com>
2023-10-02 17:02:40 -0400

    Add project files.

---
+++ Prompt.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace UPrompt
+{
+    partial class Prompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.htmlhandler = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // htmlhandler
+            // 
+            this.htmlhandler.AllowWebBrowserDrop = false;
+            this.htmlhandler.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlhandler.IsWebBrowserContextMenuEnabled = false;
+            this.htmlhandler.Location = new System.Drawing.Point(3, 24);
+            this.htmlhandler.MinimumSize = new System.Drawing.Size(20, 20);
+            this.htmlhandler.Name = "htmlhandler";
+            this.htmlhandler.ScriptErrorsSuppressed = true;
+            this.htmlhandler.ScrollBarsEnabled = false;
+            this.htmlhandler.Size = new System.Drawing.Size(794, 423);
+            this.htmlhandler.TabIndex = 0;
+            this.htmlhandler.Url = new System.Uri("", System.UriKind.Relative);
+            this.htmlhandler.Navigating += new System.Windows.Forms.WebBrowserNavigatingEventHandler(this.webBrowser1_Navigating);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.htmlhandler);
+            this.FormStyle = MaterialSkin.Controls.MaterialForm.FormStyles.ActionBar_None;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(3, 24, 3, 3);
+            this.Text = "UPrompt DEMO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser htmlhandler;
+    }
+}
+
---
+++ Prompt.cs
@@ -0,0 +1,95 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using UPrompt.Properties;
+
+namespace UPrompt
+{
+    public partial class Prompt : MaterialForm   
+    {
+        string app_path = (AppDomain.CurrentDomain.BaseDirectory).Replace(@"\","/");
+        public Prompt()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Grey900, Primary.Grey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("C:\\Users\\beah\\source\\repos\\UPrompt\\DEMO.xml");
+
+            XmlNodeList settingsList = xmlDoc.SelectNodes("//Application/Settings");
+
+            foreach (XmlNode settingNode in settingsList)
+            {
+                string name = settingNode.Attributes["Name"].Value;
+                string value = settingNode.Attributes["Value"].Value;
+
+                switch (name)
+                {
+                    case "Width":
+                    int width = int.Parse(value);
+                    this.Width = width;
+                    break;
+                
+                    case "Height":
+                        int height = int.Parse(value);
+                        this.Height = height;
+                    break;
+
+                    default:
+                        MessageBox.Show($"This property is not reconize as a valid one: {name});");
+                        break;
+                }
+                
+            }
+
+        //SET GOOD MODE FOR BROWSER (ALLOW CSS)
+        int browserVer, regVal;browserVer = 11000;regVal = browserVer << 0x10 | 0xFFFF;
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION", true);
+            key.SetValue(System.Diagnostics.Process.GetCurrentProcess().ProcessName + ".exe", regVal, RegistryValueKind.DWord);key.Close();
+
+            string html_path = $@"file:///{app_path}index.html";
+            htmlhandler.Navigate($"{(string)html_path}");
+        }
+        private void webBrowser1_Navigating(object sender, WebBrowserNavigatingEventArgs e)
+        {
+            
+            if (e.Url.ToString().Contains("LOCAL:") && e.Url.ToString().Contains("="))
+            {
+                string action_code = e.Url.ToString().Split(new string[] { "LOCAL:" },StringSplitOptions.None)[1];
+                string action_name = action_code.Split('=')[0];
+                string action_value = action_code.Split('=')[1];
+                action_value = action_value.Replace("[USER]",Environment.UserName).Replace("[DEVICE]",Environment.MachineName);
+
+                switch (action_name)
+                {
+                    case "MSG":
+                        MessageBox.Show(action_value);
+                        break;
+                    case "EXIT":
+                        Environment.Exit(int.Parse(action_value));
+                        break;
+                }
+            }
+        }
+
+    }
+}
