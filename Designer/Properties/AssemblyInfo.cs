cf5d906cc32b2a8d95a0b406f18f4f44a6949e74
TopDeveloper29 <141742981+TopDeveloper29@users.noreply.github.com>
2023-11-17 08:57:30 -0500

    Add files via upload

---
+++ Designer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Designer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Premier Tech")]
+[assembly: AssemblyProduct("Designer")]
+[assembly: AssemblyCopyright("Copyright © Premier Tech 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a564b452-c263-4d88-9821-8abe2ba9561c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.23.11.17")]
+[assembly: AssemblyFileVersion("2.23.11.17")]
---
+++ Designer/UDesignerLoader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using UPrompt.Class;
+
+namespace UPrompt.UDesigner
+{
+    public static class UDesignerLoader
+    {
+        public static void OpenNewUprompt()
+        {
+            string path = UCommon.GetVariable("SavedPath");
+            if (File.Exists(path))
+            {
+                Process upromt = new Process();
+                upromt.StartInfo.FileName = UCommon.UPrompt_exe;
+                upromt.StartInfo.WorkingDirectory = UCommon.Application_Path_Windows;
+                upromt.StartInfo.Arguments = $"/Path \"{path}\"";
+                upromt.Start();
+            }
+        }
+        public static void PreviewXML()
+        {
+            try
+            {
+                string path = UCommon.GetVariable("SavedPath");
+                UCommon.SetVariable("PreviewHtml", "");
+                UParser.ClearHtml();
+                if (File.Exists(path))
+                {
+                    
+                    string rawxml = File.ReadAllText(path);
+                    if (rawxml.ToLower().Contains("<Application>".ToLower()) && rawxml.ToLower().Contains("<View>".ToLower()) && rawxml.ToLower().Contains("</Application>".ToLower()) && rawxml.ToLower().Contains("</View>".ToLower()))
+                    {
+                        XmlDocument xmlDoc = new XmlDocument();
+                        xmlDoc.Load(path);
+                        string html = "";
+                        foreach (XmlNode childNode in xmlDoc.SelectSingleNode("/Application/View").ChildNodes)
+                        {
+                            html = UParser.GenerateHtmlFromXML(childNode.OuterXml);
+                        }
+
+                        UCommon.SetVariable("PreviewHtml", html);
+                    }
+                }
+            } catch(Exception ex) { UCommon.Error(ex.Message); }
+        }
+        public static void CallEditor(string @new = "true")
+        {
+            try
+            {
+                string path = "";
+                if (bool.Parse(@new)){ path = UCommon.GetVariable("NewPath"); File.WriteAllText(path, ""); }
+                else { path = UCommon.GetVariable("LoadPath"); }
+
+                if (Directory.Exists(Path.GetDirectoryName(path)))
+                {
+                    if (File.Exists(path))
+                    {
+                        UCommon.SetVariable("SavedPath", path);
+                        if (File.Exists(@"C:\Program Files\Notepad++\notepad++.exe"))
+                        {
+                            Process.Start(@"C:\Program Files\Notepad++\notepad++.exe", $"\"{path}\"");
+                        }
+                        else
+                        {
+                            Process.Start(path);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                UCommon.Error(ex.Message, "Save Xml Error");
+            }
+        }
+    }
+}
+
