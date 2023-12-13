9a0a4c4f1e3a80081b006175957f28e2a2503247
Hugo Beaulieu <beah@premiertech.com>
2023-12-13 09:30:51 -0500

    Update UDesigner

--- Designer/Designer.cs
+++ Designer/Designer.cs
@@ -5,10 +5,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Windows.Forms;
 using System.Xml;
-using System.Xml.Linq;
 using UPrompt.Core;
+
 
 namespace UPrompt.UDesigner
 {
